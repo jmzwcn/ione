61a37e86476d4d47caa53d389e97df1cd35927ab
Daniel <jmzwcn@gmail.com>
2019-03-28 21:02:00 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,16 +8,17 @@ declare var Swiper;
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+
   constructor() {
-    window.onload = function () {
-      //initialize swiper when document ready
-      var mySwiper = new Swiper('.swiper-container', {
-        // Optional parameters
-        direction: 'vertical',
-        effect: 'cube',
-        slidesPerView: 1,
-        cubeEffect: { shadow: false, }
-      })
-    };
+  }
+
+  ionViewDidEnter() {
+    var mySwiper = new Swiper('.swiper-container', {
+      // Optional parameters
+      direction: 'vertical',
+      effect: 'cube',
+      slidesPerView: 1,
+      cubeEffect: { shadow: false, }
+    })
   }
 }
