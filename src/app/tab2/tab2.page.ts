5c97c3762aa3d51f4503dfb68820edc62fec56d8
Daniel <jmzwcn@gmail.com>
2019-03-28 08:33:12 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
 
+declare var Swiper;
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+  constructor() {
+    window.onload = function () {
+      //initialize swiper when document ready
+      var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        direction: 'vertical',
+        effect: 'cube',
+        slidesPerView: 1,
+        cubeEffect: { shadow: false, }
+      })
+    };
+  }
+}
