51ad46f5cf4a2d7c0a0fbe7b96a08ab8dd9e21bb
Daniel <jmzwcn@gmail.com>
2019-03-28 22:11:27 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 declare var Swiper;
+
+const API_URL = environment.apiUrl;
+const API_KEY = environment.apiKey;
 
 @Component({
   selector: 'app-tab2',
@@ -8,8 +13,25 @@ declare var Swiper;
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  data: any;
+  category = 'general';
 
-  constructor() {
+  constructor(private http: HttpClient) {
+    this.load()
+  }
+
+  load() {
+    this.getData('top-headlines?country=us&category=' + this.category)
+      .subscribe(data => {
+        console.log(data);
+        this.data = data;
+        this.data.articles = this.data.articles.filter(article => article.content);
+        //loading.dismiss();
+      });
+  }
+
+  ionViewWillEnter() {
+    // this.load();
   }
 
   ionViewDidEnter() {
@@ -18,7 +40,15 @@ export class Tab2Page {
       direction: 'vertical',
       effect: 'cube',
       slidesPerView: 1,
-      cubeEffect: { shadow: false, }
+      cubeEffect: {
+        shadow: false,
+        shadowOffset: 0,
+        shadowScale: 1
+      }
     })
+  }
+
+  getData(url) {
+    return this.http.get(`${API_URL}/${url}&apiKey=${API_KEY}`);
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://newsapi.org/v2',
+  apiKey: 'af73a5ec79f34a7ba1bdef547909ed3d'
 };
 
 /*
